9a1603af2b56039a4a9e80a3ba2186e81a071883
Shredrox <93082689+Shredrox@users.noreply.github.com>
2022-11-25 12:02:22 +0200

    Added DataDictionary and DataPair classes

---
+++ LogicalExpressionInterpreter/UtilityClasses/DataDictionary.cs
@@ -0,0 +1,33 @@
+﻿namespace LogicalExpressionInterpreter.UtilityClasses
+{
+    public class DataDictionary<TKey,TValue>
+    {
+        private List<TKey> keys;
+        private List<TValue> values;
+        private List<DataPair<TKey, TValue>> items;
+
+        public DataDictionary() 
+        {
+            keys = new List<TKey>();
+            values = new List<TValue>();
+            items = new List<DataPair<TKey,TValue>>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            items.Add(new DataPair<TKey,TValue>(key, value));     
+            keys.Add(key);
+            values.Add(value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return keys.Contains(key);
+        }
+
+        public bool ContainsValue(TValue value)
+        {
+            return values.Contains(value);
+        }
+    }
+}
---
+++ LogicalExpressionInterpreter/UtilityClasses/DataPair.cs
@@ -0,0 +1,24 @@
+﻿namespace LogicalExpressionInterpreter.UtilityClasses
+{
+    public class DataPair<TKey, TValue>
+    {
+        private TKey Key;
+        private TValue Value;
+
+        public DataPair(TKey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public TKey GetKey()
+        {
+            return Key;
+        }
+
+        public TValue GetValue()
+        {
+            return Value;
+        }
+    }
+}
